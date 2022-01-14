c7d86f2016c44a5c55b9e4032fdd6d2200a7a9bc
Cameron Kirby <cameronkirby@Camerons-MBP-2.MG8702>
2022-01-14 09:22:41 -0600

    :lipstick: add home page hero slider

--- client/src/components/pages/Home/Home.js
+++ client/src/components/pages/Home/Home.js
@@ -1,31 +1,75 @@
 import React from "react"
 import { StyledHome } from './Home.styled'
 
+// Swiper
+import { Swiper, SwiperSlide } from 'swiper/react/swiper-react.js';
+import { Navigation, Pagination, Parallax } from 'swiper';
+import 'swiper/swiper.min.css'; // core Swiper
+import 'swiper/modules/navigation/navigation.min.css'; // Navigation module
+import 'swiper/modules/pagination/pagination.min.css'; // Pagination module
+
+const background1 = {
+    backgroundImage: "url('https://images.unsplash.com/photo-1578934191836-ff5f608c2228?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1349&q=80')",
+};
+
+const background2 = {
+    backgroundImage: "url('https://images.unsplash.com/photo-1579003087287-997fd4d18771?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1350&q=80')",
+};
+
+
+
 const Home = () => {
 
     return (
         <StyledHome>
-            <section className="home-landing">
-                <div className="case-study-main">
-                    
-                </div>
-                <div className="case-study-row">
-                    <div className="case-study-minor">
-                        <div className="case-study-thumbnail">
-                            <p>Hello</p>
+            <section className="hero-slider">
+                <Swiper
+                    // install Swiper modules
+                    modules={[Navigation, Pagination, Parallax]}
+                    loop={true}
+                    slidesPerView={1}
+                    navigation
+                    parallax
+                    speed={1000}
+                    pagination={{"clickable": true}}
+                    onSlideChange={() => console.log('slide change')}
+                    onSwiper={(swiper) => console.log(swiper)}
+                    >
+                    <SwiperSlide>
+                        <div className="slide-inner slide-bg-image" data-swiper-parallax="23%" style={background1}>
+                            <div className="container">
+                                <div data-swiper-parallax="300" className="slide-title">
+                                    <h2>Hello! This is a slide</h2>
+                                </div>
+                                <div data-swiper-parallax="400" className="slide-text">
+                                    <p>This is a subtitle for the slide!</p>
+                                </div>
+                                <div className="clearfix"></div>
+                                <div data-swiper-parallax="500" className="slide-btns">
+                                    <a href="#" className="theme-btn-s2">Learn more</a>
+                                    <a href="#" className="theme-btn-s3"><i className="fas fa-chevron-circle-right"></i>Get Info</a>
+                                </div>
+                            </div>
                         </div>
-                    </div>
-                    <div className="case-study-minor">
-                        <div className="case-study-thumbnail">
-                            <p>Hello</p>
+                    </SwiperSlide>
+                    <SwiperSlide>
+                        <div className="slide-inner slide-bg-image" style={background2}>
+                            <div className="container">
+                                <div data-swiper-parallax="300" className="slide-title">
+                                    <h2>Hello! This is a slide</h2>
+                                </div>
+                                <div data-swiper-parallax="400" className="slide-text">
+                                    <p>This is a subtitle for the slide!</p>
+                                </div>
+                                <div className="clearfix"></div>
+                                <div data-swiper-parallax="500" className="slide-btns">
+                                    <a href="#" className="theme-btn-s2">Learn more</a>
+                                    <a href="#" className="theme-btn-s3"><i className="fas fa-chevron-circle-right"></i>Get Info</a>
+                                </div>
+                            </div>
                         </div>
-                    </div>
-                    <div className="case-study-minor">
-                        <div className="case-study-thumbnail">
-                            <p>Hello</p>
-                        </div>
-                    </div>
-                </div>
+                    </SwiperSlide>
+                </Swiper>
             </section>
         </StyledHome>
     );
--- client/src/components/pages/Home/Home.styled.js
+++ client/src/components/pages/Home/Home.styled.js
@@ -1,33 +1,95 @@
 import styled from "styled-components"
 
 export const StyledHome = styled.div`
-    .home-landing {
+    .hero-slider {
+        /*padding-top: 500px;*/
         height: 100vh;
         background-color: var(--green);
 
-        .case-study-main {
-            height: 65vh;
-            padding: 30px var(--margin-width);
-
+        .swiper {
+            height: 100%;
         }
 
-        .case-study-row {
-            padding-right: var(--margin-width);
-            display: flex;
-
+        .swiper-slide {
+            overflow: hidden;
+        }
         
-            .case-study-minor {
-                padding-left: var(--margin-width);
-                height: 30vh;
-                width: calc(100% / 3);
+        .slide-inner {
+            width: 100%;
+            height: 100%;
+            position: absolute;
+            left: 0;
+            top: 0;
+            z-index: 1;
+            background-size: cover;
+            background-position: center;
+            display: flex;
+            justify-content: center;
+            align-items: center;
+            text-align: left;
 
-                .case-study-thumbnail {
-                    width: 100%;
-                    height: 100%;
-                    background-size: cover;
-                    background: linear-gradient(135deg, orange 50%, cyan);
-                }
+            .slide-title,
+            .slide-text,
+            .slide-btns {
+                max-width: 690px;
             }
+            /*--------------------------------------------------------------
+                #button-style
+            --------------------------------------------------------------*/
+            .theme-btn, .theme-btn-s2{
+                background-color: #ffffff;
+                font-size: 20px;
+                font-weight: 500;
+                line-height: 1.4;
+                text-align: center;
+                color:#2b3b95;
+                padding: 9px 32px;
+                border: 0; 
+                border-radius: 3px;
+                text-transform: uppercase;
+                display: inline-block;
+                line-height: initial;
+                transition:all .4s ease;
+            }
+            a {
+                text-decoration: none;
+                transition: all 0.2s ease;
+            }
+            .theme-btn-s2 {
+                background-color: rgba(255, 255, 255, 0.9);
+                color: #131e4a;
+            }
+            .theme-btn:hover, .theme-btn-s2:hover, .theme-btn:focus, .theme-btn-s2:focus, .theme-btn:active, .theme-btn-s2:active {
+                background-color:#2b3b95 ;
+                color: #fff;
+            }
+            .theme-btn-s3{
+            font-size: 16px;
+            font-weight: 500;
+            line-height: 1.5;
+            color: #ffffff;
+            text-transform: uppercase;
+            }
+            i.fa-chevron-circle-right{
+            height:22px;
+            width:22px;
+            }
+            a:hover {
+                text-decoration: none;
+            }
+            @media (max-width: 991px){
+            .theme-btn, .theme-btn-s2, .theme-btn-s3 {
+                font-size: 13px;
+                padding: 15px 25px;
+            }
+            }
+            @media (max-width: 767px){
+            .theme-btn, .theme-btn-s2{
+                padding: 13px 20px;
+                font-size: 13px;
+            }
+            }
+
         }
     }
 `
